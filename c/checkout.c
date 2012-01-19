7d3c16383ed42386c390b187bf0d6f892b1369bd
Guillaume Rose <guillaume.rose@gmail.com>
2012-01-19 20:35:28 +0100

    Première réduction

--- c/checkout.c
+++ c/checkout.c
@@ -1,18 +1,48 @@
 #include "checkout.h"
 
-int acc = 0;
+#define POMMES 0
+#define BANANES 1
+#define CERISES 2
+
+int total = 0;
+int etat[] = {0, 0, 0};
+
 void process(char *chaine) {
-    acc = acc + priceOf(chaine);
-    printf("> %i\n", acc);
+    int index = indexPour(chaine);
+    if (index < 0) {
+        return;
+    }
+
+    etat[index]++;
+    total = total + prixPour(index) + reductionPour(etat);
+    printf("> %i\n", total);
 }
 
-int priceOf(char *chaine) {
+int indexPour(char *chaine) {
 	if (strcmp(chaine, "Pommes\n") == 0)
-        return 100;
+        return POMMES;
     else if(strcmp(chaine, "Bananes\n") == 0)
-        return 150;
+        return BANANES;
     else if(strcmp(chaine, "Cerises\n") == 0)
+        return CERISES;
+
+    return -1;
+}
+
+int prixPour(int i) {
+	if (i == POMMES)
+        return 100;
+    else if(i == BANANES)
+        return 150;
+    else if(i == CERISES)
         return 75;
 
+    return 0;
+}
+
+int reductionPour(int etat[]) {
+    if (etat[CERISES] > 0 && etat[CERISES] % 2 == 0) {
+        return -30;
+    }
     return 0;
 }
