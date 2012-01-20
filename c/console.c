fe6b88b169fea0faa613cc1fc68318c644aa7c4a
Guillaume Rose <guillaume.rose@gmail.com>
2012-01-20 11:40:43 +0100

    Indentation de la console

--- c/console.c
+++ c/console.c
@@ -8,18 +8,18 @@ int main()
 {
     char *sep = ",";
     char *word, *brkt;
-	char input[1024];
+    char input[1024];
 
-	while(fgets(input, 1023, stdin) != NULL)
-	{
+    while(fgets(input, 1023, stdin) != NULL)
+    {
         input[strlen(input) - 1] = '\0';
         for (word = strtok_r(input, sep, &brkt);
-             word;
-             word = strtok_r(NULL, sep, &brkt))
+        word;
+        word = strtok_r(NULL, sep, &brkt))
         {
             process(word);
         }
-	}
+    }
     return 0;
 }
 
